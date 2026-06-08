41c308b484d17a208ca0a8861560b44a81092b02
meharsarmad786 <mahersarmad786@gmail.com>
2026-06-09 03:30:51 +0500

    Fix game cover images for PR feedback.
    Use per-game placeholder images and object-fit contain so titles are not cropped.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,6 +1,4 @@
 module GamesHelper
-  GAME_COVER_IMAGE = "board_game_images/869e8c86f98b372e3ba9a2a4ce33a4203260c04d.jpg"
-
   CONDITION_LABELS = {
     "mint" => "Mint",
     "excellent" => "Like New",
@@ -21,6 +19,6 @@ module GamesHelper
   end
 
   def game_cover_image(game)
-    GAME_COVER_IMAGE
+    "board_game_images/#{game.name}.jpg"
   end
 end
