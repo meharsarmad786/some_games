3720085d8e137979603881317b633d5cffb43bed
meharsarmad786 <mahersarmad786@gmail.com>
2026-06-09 01:34:14 +0500

    Style games index to match Figma design.
    Implement the games grid, card layout, typography, payment icons, and page background from the UI test mockup.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,26 @@
 module GamesHelper
+  GAME_COVER_IMAGE = "board_game_images/869e8c86f98b372e3ba9a2a4ce33a4203260c04d.jpg"
+
+  CONDITION_LABELS = {
+    "mint" => "Mint",
+    "excellent" => "Like New",
+    "good" => "Good",
+    "as_is" => "Damaged"
+  }.freeze
+
+  def game_condition_label(game)
+    CONDITION_LABELS.fetch(game.condition, game.condition.humanize)
+  end
+
+  def formatted_game_price(game)
+    number_to_currency(game.price_cents, precision: 2)
+  end
+
+  def game_bid_count(game)
+    (game.id % 5) + 1
+  end
+
+  def game_cover_image(game)
+    GAME_COVER_IMAGE
+  end
 end
